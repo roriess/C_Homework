12219baa5b0bd080189ba276b112e3b5e4c9d71c
Viсtoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-09-15 17:56:35 +0300

    Update flipping_the_array.c

--- Homework_2/flipping_the_array.c
+++ Homework_2/flipping_the_array.c
@@ -14,24 +14,24 @@ void reverse(int arr[], int leftEdge, int rightEdge) {
 
 int main() 
 {
-
     int m, n;
+    
     printf("Enter the digits 'm' and 'n' separated by a space: ");
     scanf("%d %d", &m, &n);
 
     int arr[m + n];
-    for (int i = 0; i < (m + n); i++) {
+
+    for (int i = 0; i < (m + n); i++)
         arr[i] = i + 1;
-    }
+
 
     reverse(arr, 0, m - 1); // flip the first part of the array
     reverse(arr, m, m + n - 1); // flip the second part of the array
     reverse(arr, 0, m + n - 1); // flip the entire array
 
-    for (int j = 0; j < (m + n); j++) {
+
+    for (int j = 0; j < (m + n); j++)
         printf("%d ", arr[j]);
-    }
 
     return 0;
-
 }
