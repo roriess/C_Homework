3ceb30925e751640ec2b1c29c88ae3ee2f2a24e1
Viсtoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-09-15 17:54:03 +0300

    fixed flipping_the_array.c

--- Homework_2/flipping_the_array.c
+++ Homework_2/flipping_the_array.c
@@ -1,20 +1,37 @@
 #include <stdio.h>
-void reverse(int arr[], int left, int right) {
-    
+
+void reverse(int arr[], int leftEdge, int rightEdge) {
+    while (leftEdge < rightEdge) {
+        int temporaryVariable = arr[leftEdge];
+        arr[leftEdge] = arr[rightEdge];
+        arr[rightEdge] = temporaryVariable;
+
+        leftEdge++;
+        rightEdge--;
+    }
 }
+
+
 int main() 
 {
+
     int m, n;
-    printf("Enter the length of the first segment 'm' and the length of the second segment 'n' separated by a space: ");
+    printf("Enter the digits 'm' and 'n' separated by a space: ");
     scanf("%d %d", &m, &n);
 
-    int arrayOfNumbers[m + n];
-
-    for (int i = m + n; i > 0; i--) {
-        arrayOfNumbers[i - 1] = i;
+    int arr[m + n];
+    for (int i = 0; i < (m + n); i++) {
+        arr[i] = i + 1;
     }
 
-    for (int j = 0; j <= m; j++) {
-        arrayOfNumbers[j]
+    reverse(arr, 0, m - 1); // flip the first part of the array
+    reverse(arr, m, m + n - 1); // flip the second part of the array
+    reverse(arr, 0, m + n - 1); // flip the entire array
+
+    for (int j = 0; j < (m + n); j++) {
+        printf("%d ", arr[j]);
     }
+
+    return 0;
+
 }
