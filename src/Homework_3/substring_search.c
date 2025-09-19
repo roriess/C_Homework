da18672f26c82257448c5c26b886a3d22b62e823
Viсtoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-09-19 21:23:49 +0300

    The algorithm has been changed

--- src/Homework_3/substring_search.c
+++ src/Homework_3/substring_search.c
@@ -9,30 +9,19 @@ int main()
 
     printf("Enter string S: ");
     fgets(s, MAX_LEN, stdin);
-    
+    s[strcspn(s, "\n")] = '\0';
+
     printf("Enter string S1: ");
     fgets(s1, MAX_LEN, stdin);
-
-
-    s[strcspn(s, "\n")] = '\0';
     s1[strcspn(s1, "\n")] = '\0';
 
-    int len = strlen(s);
-    int len1 = strlen(s1);
 
-
+    const char *p = s;
     int count = 0;
 
-    for (int i = 0; i <= (len - len1); i++) {
-        int flag = 1;
-        for (int j = 0; j < len1; j++) {
-            if (s[i + j] != s1[j]) {
-                flag = 0;
-                break;
-            }
-        }
-        if (flag)
-            count++;
+    while ((p = strstr(p, s1)) != 0) {
+        p++;
+        count++;
     }
 
     printf("The number of occurrences of S1 in S as a substring = %d", count);
