f5673700096faf249b32d5693842deb5579548fe
Viсtoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-09-20 18:41:54 +0300

    Add new file to Homework_3

---
+++ src/Homework_3/number_of_zero_elements.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#define MAX_LEN 100
+
+int main() 
+{ 
+    char arr[MAX_LEN];
+
+    printf("Enter the array elements: ");
+    fgets(arr, MAX_LEN, stdin);
+    arr[strcspn(arr, "\n")] = '\0';
+
+
+    int count = 0;
+
+    char *a = strtok(arr, " ");
+    while (a != NULL) {
+        int numberA = atoi(a);
+        
+        if (numberA == 0) 
+            count++;
+
+        a = strtok(NULL, " ");
+    }
+
+    printf("Number of zero elements is %d", count);
+    return 0;
+}
