7aa53118ccfd61bc6b7e448ba3a8a61ebb6bbbc9
Viсtoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-09-20 18:41:05 +0300

    Add new files to Homework_3

---
+++ src/Homework_3/simple_balance_of_brackets.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_LEN 100
+
+int main()
+{
+    char string[MAX_LEN];
+
+    printf("Enter an expression containing parentheses: ");
+    fgets(string, MAX_LEN, stdin);
+    string[strcspn(string, "\n")] = '\0';
+
+
+    int openingParentheses = 0;
+    int lenString = strlen(string);
+
+    for (int i = 0; i < lenString; i++) {
+        if (string[i] == '(') {
+            openingParentheses++;
+        } else if (string[i] == ')') {
+            if (openingParentheses != 0) {
+            openingParentheses--;
+            } else {
+                printf("The balance of parentheses in the line is broken");
+                return 1;
+            }
+        }
+    }
+
+    if (openingParentheses == 0) {
+        puts("The balance of parentheses in the line is maintained");
+    } else {
+        puts("The balance of parentheses in the line is broken");
+        return 1;
+    }
+    return 0;
+}
