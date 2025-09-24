4ecfed5c897751f5283701c62f02bdc1b1374f43
Viсtoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-09-24 19:12:17 +0300

    Fixed a bug with the number of '*' in fast_polynomial_calculation.c

--- src/Homework_2/fast_polynomial_calculation.c
+++ src/Homework_2/fast_polynomial_calculation.c
@@ -13,7 +13,8 @@ int main()
         printf("Enter x value: ");
         scanf("%d", &x);
 
-        int polynomialValue = ((x*x + 1)*(x*x) + x) + 1;
+        int squareX = x * x; // add squareХ
+        int polynomialValue = ((squareX + 1) * (squareX) + x) + 1; // x*x replaced by squareX
 
         printf("The value of the formula x^4 + x^3 + x^2 + x + 1 when x = %d is equal to %d\n", x, polynomialValue);
         return 0;
@@ -23,7 +24,8 @@ int main()
         printf("Enter x value: ");
         scanf("%lf", &x);
 
-        double polynomialValue = ((x*x + 1)*(x*x) + x) + 1;
+        double squareX = x * x; // add squareХ
+        double polynomialValue = ((squareX + 1) * (squareX) + x) + 1; // x*x replaced by squareX
 
         printf("The value of the formula x^4 + x^3 + x^2 + x + 1 when x = %.2f is equal to %.2f\n", x, polynomialValue);
         return 0;
