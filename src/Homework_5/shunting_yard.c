19693d83a530028478fc813bdf39313262f75f93
Viсtoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-10-06 19:44:23 +0300

    Create shunting_yard.c

---
+++ src/Homework_5/shunting_yard.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <ctype.h>
+#include <string.h>
+
+#include "implementationOfStack.c"
+#include "checkingForASymbol.c"
+
+#define MAX_LEN 100
+
+
+int main(void) 
+{
+    char mathExpression[MAX_LEN];
+    Stack *operators = new();
+    char result[MAX_LEN];
+    int end = 0;
+
+    printf("Enter an expression: ");
+    fgets(mathExpression, MAX_LEN, stdin);
+    mathExpression[strcspn(mathExpression, "\n")] = '\0';
+
+    int size = strlen(mathExpression);
+
+    for (int i = 0; i < size; i++) {
+        char element = mathExpression[i];
+
+        if (isdigit(element)) {
+            result[end++] = element;
+            continue;
+        }
+
+        if (isLeftBracket(element)) {
+            push(operators, element);
+            continue;
+        }
+
+        if (isRightBracket(element)) {
+            while (operators->top != -1 && !isLeftBracket(peek(operators)))
+                result[end++] = pop(operators);
+
+            if (operators->top != -1)
+                pop(operators);
+
+            continue;
+        }
+
+        if (isOperator(element)) {
+            while (operators->top != -1 && priority(element) <= priority(peek(operators)))
+                result[end++] = pop(operators);
+            }
+            push(operators, element);
+
+            continue;
+        }
+
+    while (operators->top != -1)
+        result[end++] = pop(operators);
+
+    printf("%s", result);
+
+    del(operators);
+    return 0;
+}
