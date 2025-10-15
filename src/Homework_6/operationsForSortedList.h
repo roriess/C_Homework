99127c9a9f8c975c7625c41e98991d0d70094386
Viсtoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-10-15 21:18:11 +0300

    Аdded structure definition

--- src/Homework_6/operationsForSortedList.h
+++ src/Homework_6/operationsForSortedList.h
@@ -3,6 +3,19 @@
 #include "operationsForSortedList.c"
 
 
+typedef struct elementFromList {
+    int data;
+    struct elementFromList* nextElement;
+} element;
+
+
+typedef struct listOfElements {
+    element* lastElement;
+    element* firstElement;
+    int listSize;
+} list;
+
+
 list* createList();
 
 void addElementToList(list* numList, int newElement);
