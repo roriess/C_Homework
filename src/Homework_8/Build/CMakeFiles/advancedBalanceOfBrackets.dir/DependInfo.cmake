c82928cedbca575e5bc0e950850ef64c000878cc
Victoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-11-05 21:08:11 +0300

    Add /Build

---
+++ src/Homework_8/Build/CMakeFiles/advancedBalanceOfBrackets.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/rorie/Desktop/C_Homework/src/Homework_8/advancedBalanceOfBrackets/advancedBalanceOfBrackets.c" "CMakeFiles/advancedBalanceOfBrackets.dir/advancedBalanceOfBrackets/advancedBalanceOfBrackets.c.obj" "gcc" "CMakeFiles/advancedBalanceOfBrackets.dir/advancedBalanceOfBrackets/advancedBalanceOfBrackets.c.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/Homework_8/Build/CMakeFiles/checkingForASymbol.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for checkingForASymbol.
+# This may be replaced when dependencies are built.
