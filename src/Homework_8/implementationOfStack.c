43ff56d8e4bb650db9aed72316e2f4930b2a15b0
Victoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-11-05 19:10:09 +0300

    Replace some files

---
+++ src/Homework_8/implementationOfStack.c
@@ -0,0 +1,50 @@
+#include "implementationOfStack.h"
+
+
+Stack* new(void) 
+{
+    Stack *stack = (Stack*)malloc(sizeof(Stack));
+    if (!stack) {
+        printf("Memory allocation error for stack structure\n");
+        return NULL;
+    }
+    stack->top = -1;
+    return stack;
+}
+
+
+void push(Stack *stack, int element)
+{
+    if (stack->top + 1 >= STACK_SIZE) {
+        printf("Error: stack is overflow\n");
+        return;
+    }
+    stack->data[++stack->top] = element;
+}
+
+
+int pop(Stack *stack) 
+{
+    if (stack->top == -1) {
+        printf("Error: stack is empty\n");
+        return -1;
+    }
+    return stack->data[(stack->top)--];
+}
+
+
+int peek(Stack *stack) 
+{
+    if (stack->top == -1) {
+        printf("Error: stack is empty\n");
+        return -1;
+    }
+    return stack->data[stack->top];
+}
+
+
+void del(Stack *stack) 
+{
+    if (stack)
+        free(stack);
+}
