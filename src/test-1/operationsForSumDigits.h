c92423cd6a3b8c5422f916988df96948f44f92e2
Victoria Parfyonova <V.V.P.28092006@yandex.ru>
2025-12-13 16:31:13 +0300

    fixed bugs and added tests

---
+++ src/test-1/operationsForSumDigits.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int sumBin(int a);
--- src/test-1/sumOfDigits.c
+++ src/test-1/sumOfDigits.c
@@ -1,30 +1,32 @@
-#include <stdio.h>
+#include "operationsForSumDigits.h"
+#include "tests_sumOfDigits.c"
 
-int sumBin(int a)
+int main(int argc, char* argv[])
 {
-    int sum = 0;
-    while (a > 0) {
-        sum += a % 2;
-        a /= 2;
+    bool flag = 1;
+    if (argc > 1) {
+        for (int i = 1; i < argc; i++) {
+            if (strcmp(argv[i], "--test") == 0) {
+                runTests();
+                flag = 0;
+                break;
+            }
+        }
     }
-    return sum;
-}
+    if (flag) {
+        int a = 1;
+        int b = 2;
 
-int main(void)
-{
-    int a = 1;
-    int b = 2;
-
-    int sumA = sumBin(a);
-    int sumB = sumBin(b);
-    if (sumA == sumB) {
-        printf("The sum of digits in binary notation is the same.\n");
-    } else {
-        if (sumA > sumB)
-            printf("%d\n", a);
-        if (sumB > sumA)
-            printf("%d\n", b);
+        int sumA = sumBin(a);
+        int sumB = sumBin(b);
+        if (sumA == sumB) {
+            printf("The sum of digits in binary notation is the same.\n");
+        } else {
+            if (sumA > sumB)
+                printf("%d\n", a);
+            if (sumB > sumA)
+                printf("%d\n", b);
+        }
     }
-
     return 0;
 }
