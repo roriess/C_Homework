60d567f1576568cc185fe678a6fd7da7df2a7e9c
Victoria Parfyonova <V.V.P.28092006@yandex.ru>
2026-02-15 14:26:16 +0300

    added counting of width and number of columns

--- src/sem-2-homework-1/operations.c
+++ src/sem-2-homework-1/operations.c
@@ -2,7 +2,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void readAndAddText(char* newFileName, char* oldFileName)
+int countCommas(const char* str)
+{
+    int count = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == ',')
+            count++;
+    }
+    return count + 1;
+}
+
+int* spacesCount(const char* str, int columnCount)
+{
+    int* data = malloc(sizeof(int) * columnCount);
+    int count = 0;
+    int columnNumber = 0;
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] != ',') {
+            count++;
+        } else {
+            if (count > data[columnNumber])
+                data[columnNumber] = count;
+            columnNumber++;
+            count = 0;
+        }
+    }
+    for (int i = 0; i < columnCount; i++)
+        printf("%d", data[i]);
+
+    return data;
+}
+
+char* stringFormatting(char* str, const int* data)
+{
+}
+
+void readAndAddText(char* newFileName, const char* oldFileName)
 {
     FILE* oldFile = fopen(oldFileName, "r");
     if (oldFile == NULL) {
@@ -15,7 +50,10 @@ void readAndAddText(char* newFileName, char* oldFileName)
         printf("File not created or found.");
         return;
     }
+
     int linesRead = 0;
+    int columnCount;
+    int firstLine = 1; // для подсчета количества столбцов
     while (!feof(oldFile)) {
         char* buffer = malloc(sizeof(char) * 100);
         const int readBytes = fscanf(oldFile, "%[^\n]", buffer);
@@ -29,6 +67,11 @@ void readAndAddText(char* newFileName, char* oldFileName)
 
         fprintf(newFile, "%s\n", buffer);
         linesRead++;
+
+        if (firstLine) {
+            columnCount = countCommas(buffer) + 1;
+            firstLine = 0;
+        }
     }
     fclose(oldFile);
     fclose(newFile);
--- src/sem-2-homework-1/operations.h
+++ src/sem-2-homework-1/operations.h
@@ -1,3 +1,10 @@
 #pragma once
 
-void readAndAddText(char* newFileName, char* oldFileName);
+// подсчет кол-ва запятых в одной строке для определения кол-ва столбцов таблицы
+int countCommas(const char* str);
+
+// подсчет ширины столбцов
+int* spacesCount(const char* str, int columnCount);
+
+// чтение данных из input.csv и добавление (с итоговым форматированием) в output.txt
+void readAndAddText(char* newFileName, const char* oldFileName);
